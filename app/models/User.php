30bc8e2638c44cc1bf6121114515e996dfeeb75d
peerapat <peerapat40595@gmail.com>
2014-04-16 11:37:04 +0700

    Resgister

--- app/models/User.php
+++ app/models/User.php
@@ -1,52 +1,22 @@
 <?php
+	use Zizaco\Confide\ConfideUser;
 
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+	class User extends ConfideUser {
+		/**
+	     * Validation rules
+	     */
+		protected $table = 'users';
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+	    public static $rules = array(
+	    	'username'=> 'required|alpha_dash',
+	        'email' => 'required|email',
+	        'password' => 'required|between:4,11|confirmed',
+	        'firstname' => 'required|alpha',
+	        'lastname' => 'required|alpha',
+	        'mobilephonenumber' =>'required|digits:10'
+	    );
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
 
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
 	}
 
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
-
-}
