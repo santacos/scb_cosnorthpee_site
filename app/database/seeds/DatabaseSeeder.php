b3e00ff10508ec3bb6049ec739d15e0558861a8a
Northnroro <northnroro@gmail.com>
2014-04-17 17:01:14 +0700

    Seeder Update Again

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,57 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('AllTableSeeder');
 	}
 
+}
+
+class AllTableSeeder extends Seeder {
+	public function run(){
+
+		DB::table('users')->delete();
+		DB::table('coperate_titles')->delete();
+//USER
+		/*User::create(array(	'username' => 'testMan',
+							'email' => 'test_man@hotmail.com',
+							'password' => '123321',
+							'firstname' => 'Test',
+							'lastname' => 'Man',
+							'mobilephonenumber' => '012343210',
+							'confirmation_code' => 'X8d7SCk0dW4M13dS',
+							'confirmed' => true,
+							'status' => 0,
+							'facebook_uid' => '1234567890'));*/
+
+		/*User::create(array(	'username' => 'testMan2',
+							'email' => 'test_man2@hotmail.com',
+							'password' => '112233',
+							'firstname' => 'Test2',
+							'lastname' => 'Man2',
+							'mobilephonenumber' => '024686420',
+							'confirmation_code' => 'AABBCCDDeeffgghh',
+							'confirmed' => false,
+							'status' => 4,
+							'facebook_uid' => '1122334455'));*/
+//COPERATE TITLE
+		CoperateTitle::create(array(	'name' => 'officer1',
+							'SLA' => 30
+							));
+		CoperateTitle::create(array(	'name' => 'officer2',
+							'SLA' => 45
+							));
+		CoperateTitle::create(array(	'name' => 'officer3',
+							'SLA' => 60
+							));
+/*
+		::create(array(	'' => '',
+							'' => '',
+							'' => '',
+							'' => '',
+							'' => '',
+							'' => '',
+							'' => '',
+							'' => '',));
+*/
+	}
 }
