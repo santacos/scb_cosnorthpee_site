258386cfc2cc196ca235410f2abc4f0448bc5afb
peerapat <peerapat40595@gmail.com>
2014-04-17 09:15:17 +0700

    Migration change

--- app/database/migrations/2014_04_16_113136_confide_setup_users_table.php
+++ app/database/migrations/2014_04_16_113136_confide_setup_users_table.php
@@ -13,7 +13,7 @@ class ConfideSetupUsersTable extends Migration {
         // Creates the users table
         Schema::create('users', function($table)
         {
-            $table->increments('id');
+            $table->increments('user_id');
             $table->string('username');
             $table->string('email');
             $table->string('password');
@@ -22,6 +22,42 @@ class ConfideSetupUsersTable extends Migration {
             $table->string('mobilephonenumber');
             $table->string('confirmation_code');
             $table->boolean('confirmed')->default(false);
+            $table->integer('idcard');
+            $table->integer('status');
+            $table->timestamps();
+        });
+        Schema::create('candidates', function($table)
+        {
+             $table->foreign('candidate_id')->references('user_id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+             $table->string('experience');
+             $table->float('profile_completeness');
+             $table->primary('user_id');
+            $table->timestamps();
+        });
+         Schema::create('candidate_requisitions', function($table)
+        {
+             $table->increments('application_id');
+             $table->foreign('requisition_id')->references('requisition_id')->on('requisitions')->onDelete('cascade')->onUpdate('cascade');
+             $table->foreign('candidate_id')->references('candidate_id')->on('candidates')->onDelete('cascade')->onUpdate('cascade');
+             $table->foreign('application_status_id')->references('application_status_id')->on('application_statuses')->onDelete('cascade')->onUpdate('cascade');
+             $table->timestamp('created_at');
+             $table->timestamp('updated_at');
+            $table->timestamps();
+        });
+          Schema::create('interview_log', function($table)
+        {
+             $table->integer('visit_number');
+             $table->foreign('application_id')->references('application_id')->on('candidate_requisitions')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamp('interview_datetime');
+            $table->primary(array('visit_number', 'application_id'));
+            $table->timestamps();
+        });
+            Schema::create('interview_log', function($table)
+        {
+             $table->integer('visit_number');
+             $table->foreign('application_id')->references('application_id')->on('candidate_requisitions')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamp('interview_datetime');
+            $table->primary(array('visit_number', 'application_id'));
             $table->timestamps();
         });
 
